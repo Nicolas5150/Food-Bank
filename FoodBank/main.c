0caa87f8d850bc36ddd5dd41fea559b1ceff87c0
Nicolas Emery <Nicolas@Nicolas-MacBook-Pro.local>
2014-10-02 11:49:02 -0400

    Initial Commit

---
+++ FoodBank/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FoodBank
+//
+//  Created by Nicolas Emery on 10/2/14.
+//  Copyright (c) 2014 Nicolas Emery. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
